ea7a461d46f9cf2b292c85b56b9ec17b4811be70
ByeongWooCho <whquddn55@gmail.com>
2021-03-10 13:51:24 +0900

    ShoppingMall Init

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import LandingPage from './views/LandingPage/LandingPage';
+import LoginPage from './views/LoginPage/LoginPage';
+import RegisterPage from './views/RegisterPage/RegisterPage';
+import NavBar from './views/NavBar/NavBar';
+import Auth from '../hoc/auth';
+
+function App() {
+  return (
+    
+    <Router>
+    <NavBar/>
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, true)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
